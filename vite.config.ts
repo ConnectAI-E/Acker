44942740196a8901188a28e0227c34bec93c492e
river <laolei@forkway.cn>
2023-07-20 19:36:17 +0800

    chore: add logo

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  // @ts-ignore
   test: {
     globals: true,
     environment: 'happy-dom',
