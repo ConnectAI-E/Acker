e62e2d07cc15d31b68c9da81a8cb3c784cf7fd20
RiverRay <laolei@forkway.cn>
2023-07-23 21:14:11 +0800

    feat: Solve mobile chat list click things

--- src/components/chat-item/index.tsx
+++ src/components/chat-item/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import classNames from 'classnames';
 import { createSearchParams, useNavigate, useSearchParams } from 'react-router-dom';
@@ -6,6 +6,7 @@ import { IconDeleteStroked } from '@douyinfe/semi-icons';
 import { Popconfirm } from '@douyinfe/semi-ui';
 import useChatList from '@/hooks/useChatList';
 import type { ChatList } from '@/global';
+import styles from './ChatItem.module.less';
 
 interface ChatItemProps {
   chat: ChatList;
@@ -24,8 +25,6 @@ const ChatItem: React.FC<ChatItemProps> = function ChatItem({ chat }) {
   const checked: boolean = chat.chatId === query.get('chatId');
   const tileChatDynamicCls: Record<string, boolean> = { '!bg-white dark:!bg-[#2e333d]': checked };
 
-  const [show, setShow] = useState(false);
-
   const { handleDelete: onDelete } = useChatList();
 
   const { assistant } = chat;
@@ -37,24 +36,16 @@ const ChatItem: React.FC<ChatItemProps> = function ChatItem({ chat }) {
     });
   };
 
-  const handleStopPropagation = (event: React.MouseEvent<Element, MouseEvent>) => {
-    event.stopPropagation();
-  };
-
   const handleDelete = (event: React.MouseEvent<Element, MouseEvent>) => {
-    handleStopPropagation(event);
-    if (show || checked) {
-      onDelete(chat.chatId);
-    }
+    event.stopPropagation();
+    onDelete(chat.chatId);
   };
 
   return (
     <div
       key={chat.chatId}
-      className={classNames('w-[98%] h-[80px] px-[20px] py-[16px] mt-[8px] ', tileChatCls, tileChatDynamicCls)}
+      className={classNames('w-[98%] h-[80px] px-[14px] py-[16px] mt-[8px]', tileChatCls, tileChatDynamicCls, styles.chatItem)}
       onClick={handleClick}
-      onMouseEnter={() => setShow(true)}
-      onMouseLeave={() => setShow(false)}
     >
       <div
         className="flex-shrink-0 h-[48px] w-[48px] flex items-center justify-center rounded mr-[8px]"
@@ -82,13 +73,13 @@ const ChatItem: React.FC<ChatItemProps> = function ChatItem({ chat }) {
             onConfirm={handleDelete}
           >
             <IconDeleteStroked
-              className={classNames('flex-shrink-0 opacity-0 hover:opacity-100', { 'opacity-80': show || checked })}
-              onClick={handleStopPropagation}
+              className={classNames('flex-shrink-0 opacity-0', styles.deleteIcon, { 'opacity-80': checked })}
+              onClick={(e) => e.stopPropagation()}
             />
           </Popconfirm>
         </div>
         <div className="w-full text-overflow-l1 text-[12px] opacity-50">
-          {chat.data[chat.data.length - (assistant?.model === 'midjourney' ? 2 : 1)]?.value || `[${t('empty chat')}]`}
+          { chat.data[chat.data.length - (assistant?.model === 'midjourney' ? 2 : 1)]?.value || `[${t('empty chat')}]` }
         </div>
       </div>
     </div>
