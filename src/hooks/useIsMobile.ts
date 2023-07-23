d38c104bcc2123c674143d33fd638b66c3809206
yi yi <cjjj3845@gmail.com>
2023-07-23 21:24:53 +0800

    upd: ipad useragent
    (cherry picked from commit 841e19003318939846f4ac9b65e33940f919ec5f)

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -1,12 +1,16 @@
 function isMobile() {
   let flag = false;
-  const { userAgent } = navigator;
+  const { userAgent, maxTouchPoints } = navigator;
   const mobileAgents = ['Android', 'iPhone', 'iPad', 'iPod', 'BlackBerry', 'Windows Phone'];
   for (let i = 0; i < mobileAgents.length; i += 1) {
     if (userAgent.includes(mobileAgents[i])) {
       flag = true;
       break;
     }
+  }
+  // 这里是判断ipad的类型，ipados13以上的useragent判断不出来是ipad还是mac
+  if (userAgent.toLowerCase().includes('macintosh') && maxTouchPoints && maxTouchPoints > 2) {
+    flag = true;
   }
   return flag;
 }
