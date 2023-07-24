1c573869a0ac5c277e0da4c6b670e14c8ffddf5a
yi yi <cjjj3845@gmail.com>
2023-07-24 12:46:42 +0800

    fix: 修复主题色等相关问题
    (cherry picked from commit c6eede76b5e82216959b1ea8d98703fd71ab1f31)

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,6 +1,15 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
-type Mode = 'dark' | 'light' | 'auto';
+type Theme = 'dark' | 'light';
+type Mode = Theme | 'auto';
+
+type UseThemeArray = [Mode, (mode: Mode) => void, Theme];
+
+interface UseThemeObject {
+  mode: Mode;
+  currentTheme: Theme;
+  onChange: (mode: Mode) => void;
+}
 
 const changeTheme = (theme: 'dark' | 'light') => {
   const html = document.getElementsByTagName('html')[0];
@@ -13,12 +22,11 @@ const changeTheme = (theme: 'dark' | 'light') => {
   }
 };
 
-function useTheme(): [Mode, (value: Mode) => void] {
+function useTheme(): UseThemeArray & UseThemeObject {
   const [mode, setMode] = useState<Mode>(() => {
     if (typeof window.matchMedia === 'function') {
       const theme = localStorage?.getItem('theme') as Mode;
       const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      if (theme === 'auto') return darkMode ? 'dark' : 'light';
       if (theme) return theme;
       const html = document.getElementsByTagName('html')[0];
       if (html.className.includes('dark')) return 'dark';
@@ -28,18 +36,35 @@ function useTheme(): [Mode, (value: Mode) => void] {
     return 'light'; // 表示不支持暗色模式
   });
 
+  const [currentTheme, setCurrentTheme] = useState<Theme>(() => {
+    if (mode === 'auto') {
+      const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      return darkMode ? 'dark' : 'light';
+    }
+    return mode;
+  });
+
   const handleChangeMode = useCallback((value: Mode) => {
     setMode(value);
     localStorage?.setItem('theme', value);
     if (value === 'dark' || value === 'light') {
       changeTheme(value);
+      setCurrentTheme(value);
     } else {
       const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      changeTheme(darkMode ? 'dark' : 'light');
+      const currentMode = darkMode ? 'dark' : 'light';
+      changeTheme(currentMode);
+      setCurrentTheme(currentMode);
     }
   }, []);
 
-  return [mode, handleChangeMode];
+  const ret = [mode, handleChangeMode, currentTheme] as UseThemeArray & UseThemeObject;
+
+  ret.mode = mode;
+  ret.currentTheme = currentTheme;
+  ret.onChange = handleChangeMode;
+
+  return ret;
 }
 
 export default useTheme;
